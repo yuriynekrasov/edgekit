7d1bbbdd8cdb0710a0fdb8d289c0852901c83856
Kondratski Vitaliy <kondratskivd@gmail.com>
2021-04-19 22:09:14 +0300

    added login form with fake api

---
+++ src/components/Login.vue
@@ -0,0 +1,47 @@
+<template>
+  <form @submit.prevent="onSubmit">
+    <div>
+      <label for="username">Username</label>
+      <input
+        id="username"
+        v-model="form.username"
+        placeholder="Username"
+        type="text">
+    </div>
+    <div>
+      <label for="password">Username</label>
+      <input
+        id="password"
+        v-model="form.password"
+        placeholder="Username"
+        type="text">
+    </div>
+    <p>{{ userStore.state.error }}</p>
+    <button>Login</button>
+  </form>
+</template>
+
+<script>
+import { defineComponent, reactive } from "vue";
+import userStore from '@/store/user'
+
+export default defineComponent({
+  setup() {
+    const form = reactive({
+      username: '',
+      password: ''
+    })
+
+    const onSubmit = () => {
+      userStore.login(form.username, form.password)
+      form.username = ''
+      form.password = ''
+    }
+    return { form, userStore, onSubmit }
+  }
+})
+</script>
+
+<style scoped>
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,18 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <Login/>
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+import Login from '@/components/Login.vue'; // @ is an alias to /src
 
 export default defineComponent({
   name: 'Home',
   components: {
-    HelloWorld,
+    Login,
   },
 });
 </script>
